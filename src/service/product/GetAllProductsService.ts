c54fd1732413bfb4e27bb4744e3e9d676774e23c
FMiciano <fabio.fferreira@mercadolivre.com>
2023-03-07 11:42:09 -0300

    finalzia parte de produtos

--- src/service/product/GetAllProductsService.ts
+++ src/service/product/GetAllProductsService.ts
@@ -3,15 +3,13 @@ import prismaClient from "../../../prisma"
 class GetAllProductsService {
     async execute() {
         const products = await prismaClient.product.findMany({
-            select: {
-                id: true,
-                title: true,
-                info: true,
-                type: true,
-                link_type: true,
-                link_action: true,
-                image: true,
-                category_id: true
+            include: {
+                category: {
+                    select: { 
+                        id: true,
+                        title: true
+                    }
+                }
             }
         })
 
